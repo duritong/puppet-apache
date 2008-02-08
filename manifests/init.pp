0526a4edb25f5b985cfcd921b6a5cd1e70ee6d3a
mh <mh@immerda.ch>
2008-02-08 14:52:53 +0000

    refactored apache class

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,44 +2,21 @@
 # 2008 - admin(at)immerda.ch
 # License: GPLv3
 
-
 class apache {
     package { 'apache':
-        name => $operatingsystem ? {
-            centos => 'httpd',
-            default => 'apache'
-        },
-        category => $operatingsystem ? {
-            gentoo => 'www-servers',
-            default => '',
-        },
-            ensure => present,
-        }
-
-    case $operatingsystem {
-        centos: {
-            package { 'mod_ssl':
-                name => 'mod_ssl',
-                    ensure => present,
-            }
-        }
+        name => 'apache'
+        ensure => present,
     }
 
     service { apache:
-        name => $operatingsystem ? {
-            centos => 'httpd',
-            default => 'apache2'
-        },
+        name => 'apache2'
         enable => true,
         ensure => running,
         require => Package[apache],
     }
 
-    file { 'default_index':
-        path => $operatingsystem ? {
-            centos => '/var/www/html/index.html',
-            default => '/var/www/localhost/index.html'
-        },
+    file { 'default_apache_index':
+        path => '/var/www/localhost/index.html'
         ensure => file,
         owner => 'root',
         group => 0,
@@ -49,19 +26,41 @@ class apache {
     }
 
     case $operatingsystem {
-        centos:  { include apache_centos }
+        centos: { include apache::centos }
+        gentoo: { include apache::gentoo }
     }
 }
 
 
 
 ### distro specific stuff
-class apache_centos {
-        file {'/etc/httpd/vhosts.d':
-                ensure => directory,
-                owner => root,
-                group => 0,
-                mode => 700,
-        }
+class apache::centos {
+    Package[apache]{
+        name => 'httpd',
+    } 
+    Service[apache]{
+        name => 'httpd'
+    }
+    package { 'mod_ssl':
+        name => 'mod_ssl',
+        ensure => present,
+        require => Package[apache],
+    }
+
+    File[default_apache_index]{
+        path => '/var/www/html/index.html',
+    }
+
+    file {'/etc/httpd/vhosts.d':
+        ensure => directory,
+        owner => root,
+        group => 0,
+        mode => 750,
+    }
 }
 
+class apache::gentoo {
+    Package[apache]{
+        category => 'www-servers',
+    } 
+}
