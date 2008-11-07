a33e779091a16b70f627a7ccc6f0efc67718c2b3
mh <mh@immerda.ch>
2008-11-07 18:07:56 +0000

    use rather default shell

--- files/openbsd/bin/restart_apache_ssl.sh
+++ files/openbsd/bin/restart_apache_ssl.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
 ignoreout='Processing config'
 apachectl restart 2>&1 | (egrep -v -e "_default_ VirtualHost overlap on port 443" -e "$ignoreout" -e "/usr/sbin/apachectl restart: httpd restarted" || true )
