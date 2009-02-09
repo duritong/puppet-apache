6fca1962614ce432068cf5e72ec085022844e560
andreas <andreas@immerda.ch>
2009-02-09 19:01:07 +0000

    runas_user documentroot_owner

--- manifests/defines/vhost_varieties.pp
+++ manifests/defines/vhost_varieties.pp
@@ -232,8 +232,8 @@ define apache::vhost::php::joomla(
     git::clone{"git_clone_$name":
         git_repo => "git://git.immerda.ch/ijoomla.git",
         projectroot => $documentroot,
-        runas_user => $owner,
-        runas_group => $group
+        runas_user => $documentroot_owner,
+        runas_group => $documentroot_group
     }
     # create and/or put correct permissions
     apache::vhost::webdir{$name:
