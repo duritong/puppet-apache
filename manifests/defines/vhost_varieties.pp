b40babb4cc007e498d70a3fd6ec208e61adfab68
mh <mh@immerda.ch>
2009-03-15 20:36:03 +0000

    added stuff for wordpress / cleanup
    - vhost stuff for wordpress
    - cleaned up a bit with new language features

--- manifests/defines/vhost_varieties.pp
+++ manifests/defines/vhost_varieties.pp
@@ -5,6 +5,7 @@
 # - apache::vhost::static
 # - apache::vhost::php
 # - apache::vhost::joomla
+# - apache::vhost::wordpress
 # - apache::vhost::cgi TODO
 # - apache::vhost::modperl TODO
 # - apache::vhost::modpython TODO
@@ -224,28 +225,23 @@ define apache::vhost::php::joomla(
         documentroot_mode => $documentroot_mode,
     }
 
-    case $ensure {
-        absent: { info("don't need to remove additional managed files for ${name} on ${fqdn}")}
-        default: {
-            if $manage_directories {
-                apache::file::rw{ [ "$documentroot/administrator/backups",
-                                    "$documentroot/administrator/components",
-                                    "$documentroot/administrator/language",
-                                    "$documentroot/administrator/modules",
-                                    "$documentroot/administrator/templates",
-                                    "$documentroot/components",
-                                    "$documentroot/images",
-                                    "$documentroot/language",
-                                    "$documentroot/media",
-                                    "$documentroot/modules",
-                                    "$documentroot/plugins",
-                                    "$documentroot/templates",
-                                    "$documentroot/cache",
-                                    "$documentroot/administrator/cache" ]:
-                    owner => $documentroot_owner,
-                    group => $documentroot_group,
-                }
-            }
+    if ($ensure != 'absent') and $manage_directories {
+        apache::file::rw{ [ "$documentroot/administrator/backups",
+                            "$documentroot/administrator/components",
+                            "$documentroot/administrator/language",
+                            "$documentroot/administrator/modules",
+                            "$documentroot/administrator/templates",
+                            "$documentroot/components",
+                            "$documentroot/images",
+                            "$documentroot/language",
+                            "$documentroot/media",
+                            "$documentroot/modules",
+                            "$documentroot/plugins",
+                            "$documentroot/templates",
+                            "$documentroot/cache",
+                            "$documentroot/administrator/cache" ]:
+            owner => $documentroot_owner,
+            group => $documentroot_group,
         }
     }
 
@@ -294,6 +290,130 @@ define apache::vhost::php::joomla(
         }
     }
 }
+# run_mode: 
+#   - normal: nothing special (*default*)
+#   - itk: apache is running with the itk module 
+#          and run_uid and run_gid are used as vhost users
+# run_uid: the uid the vhost should run as with the itk module
+# run_gid: the gid the vhost should run as with the itk module
+define apache::vhost::php::wordpress(
+    $ensure = present,
+    $domain = 'absent',
+    $domainalias = 'absent',
+    $path = 'absent',
+    $owner = root,
+    $group = 0,
+    $documentroot_owner = apache,
+    $documentroot_group = 0,
+    $documentroot_mode = 0750,
+    $run_mode = 'normal',
+    $run_uid = 'absent',
+    $run_gid = 'absent',
+    $allow_override = 'None',
+    $php_upload_tmp_dir = 'absent',
+    $php_session_save_path = 'absent',
+    $do_includes = false,
+    $options = 'absent',
+    $additional_options = 'absent',
+    $default_charset = 'absent',
+    $mod_security = true,
+    $ssl_mode = false,
+    $vhost_mode = 'template',
+    $vhost_source = 'absent',
+    $vhost_destination = 'absent',
+    $htpasswd_file = 'absent',
+    $htpasswd_path = 'absent',
+    $manage_config = true,
+    $config_webwriteable = false,
+    $manage_directories = true
+){
+
+    include apache::joomla
+
+    apache::vhost::phpdirs{"${name}":
+        ensure => $ensure,
+        php_upload_tmp_dir => $php_upload_tmp_dir,
+        php_session_save_path => $php_session_save_path,
+        documentroot_owner => $documentroot_owner,
+        documentroot_group => $documentroot_group,
+        documentroot_mode => $documentroot_mode,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+    }
+
+    $real_path = $path ? {
+        'absent' => $operatingsystem ? {
+            openbsd => "/var/www/htdocs/${name}",
+            default => "/var/www/vhosts/${name}"
+        },
+        default => "${path}"
+    }
+    $documentroot = "${real_path}/www"
+
+    # create and/or put correct permissions
+    apache::vhost::webdir{$name:
+        ensure => $ensure,
+        path => $real_path,
+        owner => $owner,
+        group => $group,
+        documentroot_owner => $documentroot_owner,
+        documentroot_group => $documentroot_group,
+        documentroot_mode => $documentroot_mode,
+    }
+
+    if ($ensure != 'absent') and $manage_directories {
+        apache::file::rw{ "$documentroot/wp-content":
+            owner => $documentroot_owner,
+            group => $documentroot_group,
+        }
+    }
+
+    # create vhost configuration file
+    apache::vhost{$name:
+        ensure => $ensure,
+        path => $path,
+        template_mode => 'php_wordpress',
+        vhost_mode => $vhost_mode,
+        vhost_source => $vhost_source,
+        vhost_destination => $vhost_destination,
+        domain => $domain,
+        domainalias => $domainalias,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+        run_gid => $run_gid,
+        allow_override => $allow_override,
+        do_includes => $do_includes,
+        options => $options,
+        additional_options => $additional_options,
+        default_charset => $default_charset,
+        php_upload_tmp_dir => $php_upload_tmp_dir,
+        php_session_save_path => $php_session_save_path,
+        ssl_mode => $ssl_mode,
+        htpasswd_file => $htpasswd_file,
+        htpasswd_path => $htpasswd_path,
+        mod_security => $mod_security,
+    }
+
+    if $manage_config {
+        apache::vhost::file::documentrootfile{"wordpressconfigurationfile_${name}":
+            documentroot => $documentroot,
+            filename => 'wp-config.php',
+            thedomain => $name,
+            owner => $documentroot_owner,
+            group => $documentroot_group,
+        }
+        if $config_webwriteable {
+            Apache::Vhost::File::Documentrootfile["wordpressconfigurationfile_${name}"]{
+                mode => 0660,
+            }
+        } else {
+            Apache::Vhost::File::Documentrootfile["wordpressconfigurationfile_${name}"]{
+                mode => 0440,
+            }
+        }
+    }
+}
+
 
 # run_mode: 
 #   - normal: nothing special (*default*)
