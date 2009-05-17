06f123d9254444ec7452bcc6d768593a5507a602
mh <mh@immerda.ch>
2009-05-17 22:44:25 +0200

    added package again

--- manifests/centos/itk.pp
+++ manifests/centos/itk.pp
@@ -1,6 +1,9 @@
 # http://hostby.net/home/2008/07/12/centos-5-and-mpm-itk/
 class apache::centos::itk inherits apache::centos {
-    include ::apache::package::itk
+    include ::apache::base::itk
+    Package['apache']{
+        name => 'httpd-itk',
+    }
     File['/etc/sysconfig/httpd']{
       source => [ "puppet://$server/files/apache/sysconfig/${fqdn}/httpd.itk",
                   "puppet://$server/files/apache/sysconfig/httpd.itk",
