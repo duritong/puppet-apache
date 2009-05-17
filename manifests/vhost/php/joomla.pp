d4fb8d182a1877a8b6db6e2770660a87de60eae3
mh <mh@immerda.ch>
2009-05-17 22:48:17 +0200

    fixing comment

--- manifests/vhost/php/joomla.pp
+++ manifests/vhost/php/joomla.pp
@@ -1,5 +1,5 @@
-
-rmal: nothing special (*default*)
+# run_mode:
+#   - normal: nothing special (*default*)
 #   - itk: apache is running with the itk module
 #          and run_uid and run_gid are used as vhost users
 # run_uid: the uid the vhost should run as with the itk module
