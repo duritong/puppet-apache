1f235768659e1ff838c99b4d89b06e08bd897bea
mh <mh@immerda.ch>
2009-05-23 01:24:39 +0200

    cherry picked f863b7bb49f42841c36776aa61f21e64e2c826eb

--- manifests/centos/itk.pp
+++ manifests/centos/itk.pp
@@ -1,9 +1,6 @@
 # http://hostby.net/home/2008/07/12/centos-5-and-mpm-itk/
 class apache::centos::itk inherits apache::centos {
-    include ::apache::base::itk
-    Package['apache']{
-        name => 'httpd-itk',
-    }
+    include ::apache::package::itk
     File['/etc/sysconfig/httpd']{
       source => [ "puppet://$server/files/apache/sysconfig/${fqdn}/httpd.itk",
                   "puppet://$server/files/apache/sysconfig/httpd.itk",
