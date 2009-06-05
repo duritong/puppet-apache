4ed0bf57376f2f7c73e66b159adfbd0aa91aaa96
Simon Josi <josi@puzzle.ch>
2009-06-05 22:30:36 +0200

    add passenger vhost variant

---
+++ manifests/vhost/passenger.pp
@@ -0,0 +1,73 @@
+# run_uid: the uid the vhost should run as with the mod_passenger module
+# run_gid: the gid the vhost should run as with the mod_passenger module
+define apache::vhost::passenger(
+    $ensure = present,
+    $domain = 'absent',
+    $domainalias = 'absent',
+    $server_admin = 'absent',
+    $path = 'absent',
+    $manage_webdir = true,
+    $manage_docroot = true,
+    $template_mode = 'php',
+    $owner = root,
+    $group = apache,
+    $documentroot_owner = apache,
+    $documentroot_group = 0,
+    $documentroot_mode = 0640,
+    $run_mode = 'normal',
+    $run_uid = 'absent',
+    $run_gid = 'absent',
+    $allow_override = 'None',
+    $do_includes = false,
+    $options = 'absent',
+    $additional_options = 'absent',
+    $default_charset = 'absent',
+    $mod_security = true,
+    $ssl_mode = false,
+    $vhost_mode = 'template',
+    $vhost_source = 'absent',
+    $vhost_destination = 'absent',
+    $htpasswd_file = 'absent',
+    $htpasswd_path = 'absent'
+){
+    if $manage_webdir {
+      # create webdir
+      ::apache::vhost::webdir{$name:
+        ensure => $ensure,
+        path => $path,
+        owner => $owner,
+        group => $group,
+        run_mode => 'normal',
+        manage_docroot => $manage_docroot,
+        documentroot_owner => $documentroot_owner,
+        documentroot_group => $documentroot_group,
+        documentroot_mode => $documentroot_mode,
+      }
+    }
+
+    # create vhost configuration file
+    ::apache::vhost{$name:
+        ensure => $ensure,
+        path => $path,
+        template_mode => $template_mode,
+        vhost_mode => $vhost_mode,
+        vhost_source => $vhost_source,
+        vhost_destination => $vhost_destination,
+        domain => $domain,
+        domainalias => $domainalias,
+        server_admin => $server_admin,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+        run_gid => $run_gid,
+        allow_override => $allow_override,
+        do_includes => $do_includes,
+        options => $options,
+        additional_options => $additional_options,
+        default_charset => $default_charset,
+        ssl_mode => $ssl_mode,
+        htpasswd_file => $htpasswd_file,
+        htpasswd_path => $htpasswd_path,
+        mod_security => $mod_security,
+    }
+}
+
