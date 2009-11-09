57da0ebe11e5b6f99dc96c13845e4b3205442df1
Andreas <andreas@immerda.ch>
2009-11-09 18:00:37 +0100

    safe_mode_bin links

---
+++ manifests/vhost/php/safe_mode_bin.pp
@@ -0,0 +1,16 @@
+# run_mode:
+#   - normal: nothing special (*default*)
+#   - itk: apache is running with the itk module
+#          and run_uid and run_gid are used as vhost users
+# run_uid: the uid the vhost should run as with the itk module
+# run_gid: the gid the vhost should run as with the itk module
+define apache::vhost::php::safe_mode_bin(
+   $path
+){
+    $substr=regsubst($name,'^.*\/','','G')
+    $real_path = "$path/$substr"
+    apache::file::link{ "$real_path":
+         target => regsubst($name,'^.*_','')
+    }
+}
+
