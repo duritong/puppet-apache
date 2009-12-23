124b25746ae4892d6ba4db24609320a5a782ac0d
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2009-12-23 14:57:14 -0500

    fix debian support for itk and use correct httpd username in managed webdir

--- manifests/debian/itk.pp
+++ manifests/debian/itk.pp
@@ -1,5 +1,8 @@
 class apache::debian::itk inherits apache::debian {
-    include ::apache::base::itk
+    File['htpasswd_dir']{
+        group => 0,
+	mode => 0644,
+    }
     Package['apache']{
         name => 'apache2-mpm-itk',
     }
