ecdcd0d0612af14827fda05da33fdf9f5e2167af
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2010-02-03 09:58:41 -0500

    in ssl::debian, enable mod_ssl and remove line for ports.conf as this breaks the package-supplied config, which already includes the proper directives for port 443

--- manifests/ssl/debian.pp
+++ manifests/ssl/debian.pp
@@ -1,9 +1,3 @@
 class apache::ssl::debian inherits apache::ssl::base {
-    line { 'apache_debian_ssl_port':
-        file => "${apache::debian::config_dir}/ports.conf",
-        line => "Listen 443",
-        ensure => present,
-        require => Package['apache'],
-        notify => Service['apache'],
-    }
+    apache::debian::module { 'ssl': ensure => present }
 }
