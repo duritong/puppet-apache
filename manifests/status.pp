f534d097fb2ac5c49ce71f8a47d4de97efd22e28
mh <mh@immerda.ch>
2010-07-20 07:43:54 +0200

    clean up some possible loops

--- manifests/status.pp
+++ manifests/status.pp
@@ -1,6 +1,6 @@
 # manifests/status.pp
 
-class apache::status inherits apache {
+class apache::status {
     case $operatingsystem {
         centos: { include apache::status::centos }
         defaults: { include apache::status::base }
