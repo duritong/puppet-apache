6729542e677e210dce5f74c287c825b499a5d055
mh <mh@immerda.ch>
2010-08-16 23:02:02 +0200

    use modules url

--- manifests/ssl/itk_plus/centos.pp
+++ manifests/ssl/itk_plus/centos.pp
@@ -1,7 +1,7 @@
 class apache::ssl::itk_plus::centos inherits apache::ssl::centos {
   include apache::ssl::itk::centos
   Apache::Config::Global['ssl.conf']{
-    source => "apache/itk_plus/conf.d/${operatingsystem}/ssl.conf",
+    source => "modules/apache/itk_plus/conf.d/${operatingsystem}/ssl.conf",
   }
 
   Apache::Config::Global['00-listen-ssl.conf']{
