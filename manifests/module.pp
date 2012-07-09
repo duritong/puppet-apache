c05d4777452966603d941e899ad240e40792e8c6
Micah Anderson <micah@riseup.net>
2012-07-08 21:40:36 -0400

    there is a apache::centos::module, apache::gentoo::module, and apache::debian::module which means you have to specify the operating system in the class that you called for deployment of the module. so I abstracted that by creating the class apache::module in manifests/module.pp which just did an  fact check and then depending on the result, called the correct class for module deployment

---
+++ manifests/module.pp
@@ -0,0 +1,27 @@
+class apache::module (
+  $ensure = 'present', $source = '',
+  $destination = '', $module = '', $package_name = '' ) 
+{
+
+  case $operatingsystem {
+    centos: {
+      apache::centos::module { "$module":
+        ensure => $ensure, source => $source,
+        destination => $destination
+      }
+    }
+    gentoo: {
+      apache::gentoo::module { "$module":
+        ensure => $ensure, source => $source,
+        destination => $destination
+      }
+    }
+    'debian','ubuntu': {
+      apache::debian::module { "$module":
+        ensure => $ensure, package_name => $package_name
+      }
+    }
+    default: {
+      err("Your operating system does not have a module deployment mechanism defined") }
+  }
+}
