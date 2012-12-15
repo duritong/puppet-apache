3f0da2ac4651340a459194e450dbe8be6b1b8cab
mh <mh@immerda.ch>
2012-12-15 17:29:45 +0100

    use common define

--- manifests/sftponly/centos.pp
+++ manifests/sftponly/centos.pp
@@ -2,12 +2,9 @@
 # user for access
 class apache::sftponly::centos {
   require user::groups::sftponly
-  augeas{"add_apache_to_group_sftponly":
-    context => "/files/etc/group",
-    changes => [ "ins user after sftponly/user[last()]",
-      "set sftponly/user[last()] apache" ],
-    onlyif  => "match sftponly/*[../user='apache'] size == 0",
-    require => [ Package['apache'], Group['sftponly'] ],
-    notify  =>  Service['apache'],
+  user::groups::manage_user{'apache':
+    group   => 'sftponly',
+    require => Package['apache'],
+    notify  => Service['apache'],
   }
 }
