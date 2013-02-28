80c3e890becd31f704f3409094d6da1c137b844e
mh <mh@immerda.ch>
2013-02-28 20:32:41 +0100

    add munin plugin to this module

--- manifests/status.pp
+++ manifests/status.pp
@@ -1,12 +1,12 @@
-# manifests/status.pp
-
+# enable apache status page
+# manage munin plugins if requested
 class apache::status {
   case $::operatingsystem {
     centos: { include apache::status::centos }
     defaults: { include apache::status::base }
   }
   if $apache::manage_munin {
-    include munin::plugins::apache
+    include apache::munin
   }
 }
 
