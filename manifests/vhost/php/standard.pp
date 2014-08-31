f7ef79b62be080b66cfb68cb6102cb3be2ea07d5
mh <mh@immerda.ch>
2014-08-31 13:35:48 +0200

    add scl55 support

--- manifests/vhost/php/standard.pp
+++ manifests/vhost/php/standard.pp
@@ -238,6 +238,12 @@ define apache::vhost::php::standard(
             binary          => '/opt/rh/php54/root/usr/bin/php-cgi',
             additional_cmds => 'source /opt/rh/php54/enable',
           }
+        } elsif $php_installation == 'scl55' {
+          require php::scl::php55
+          Mod_fcgid::Starter[$name]{
+            binary          => '/opt/rh/php55/root/usr/bin/php-cgi',
+            additional_cmds => 'source /opt/rh/php55/enable',
+          }
         }
       }
       default: { include ::php }
