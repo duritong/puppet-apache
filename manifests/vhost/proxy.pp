8949089649b9b0ac6cd619e6bde8a25bb45e214c
mh <mh@immerda.ch>
2016-06-11 12:27:54 +0200

    change naming of parameter to something that makes more sense AND fix permissions

--- manifests/vhost/proxy.pp
+++ manifests/vhost/proxy.pp
@@ -4,7 +4,7 @@
 # - ensure: wether this vhost is `present` or `absent`
 # - domain: the domain to redirect (*name*)
 # - domainalias: A list of whitespace seperated domains to redirect
-# - target_url: the url to be proxied. Note: We don't want http://example.com/foobar only example.com/foobar
+# - backend: the url to be proxied. Note: We don't want http://example.com/foobar/ only http://example.com/foobar
 # - server_admin: the email that is shown as responsible
 # - ssl_mode: wether this vhost supports ssl or not
 #   - false: don't enable ssl for this vhost (default)
@@ -20,7 +20,7 @@
 #   - semianonym: Don't log ips for CustomLog, log normal ErrorLog
 #
 define apache::vhost::proxy(
-  $target_url,
+  $backend,
   $ensure                          = present,
   $configuration                   = {},
   $domain                          = 'absent',
@@ -28,6 +28,7 @@ define apache::vhost::proxy(
   $htpasswd_file                   = 'absent',
   $server_admin                    = 'absent',
   $logmode                         = 'default',
+  $logpath                         = 'absent',
   $mod_security                    = false,
   $ssl_mode                        = false,
   $mod_security_relevantonly       = true,
@@ -35,9 +36,12 @@ define apache::vhost::proxy(
   $mod_security_additional_options = 'absent',
   $additional_options              = 'absent'
 ){
-  $logpath = $::operatingsystem ? {
-    'CentOS' => '/var/log/httpd',
-    default  => '/var/log/apache2'
+  $real_logpath = $logpath ? {
+    'absent' => $::operatingsystem ? {
+      'CentOS' => '/var/log/httpd',
+      default  => '/var/log/apache2'
+    },
+    default => $logpath,
   }
   # create vhost configuration file
   # we use the options field as the target_url
@@ -51,14 +55,15 @@ define apache::vhost::proxy(
     htpasswd_file                   => $htpasswd_file,
     domainalias                     => $domainalias,
     server_admin                    => $server_admin,
-    logpath                         => $logpath,
+    logpath                         => $real_logpath,
     logmode                         => $logmode,
     run_mode                        => 'normal',
     mod_security                    => $mod_security,
     mod_security_relevantonly       => $mod_security_relevantonly,
     mod_security_rules_to_disable   => $mod_security_rules_to_disable,
     mod_security_additional_options => $mod_security_additional_options,
-    options                         => $target_url,
+    options                         => $backend, # we abuse the options param
+                                                  # to be used in the template
     ssl_mode                        => $ssl_mode,
     additional_options              => $additional_options,
   }
