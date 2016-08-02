234f3a11cdbe2cef54445d8249a05e0e88d9202c
mh <mh@immerda.ch>
2016-08-02 22:47:18 +0200

    fix seltype for EL7

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -4,9 +4,14 @@ class apache::munin {
     include perl::extensions::libwww
   }
 
+  if $::operatingsystem == 'CentOS' and versioncmp($::operatingsystemmajrelease,'6') > 0 {
+    $seltype = 'services_munin_plugin_exec_t'
+  } else {
+    $seltype = 'munin_services_plugin_exec_t'
+  }
   munin::plugin{ [ 'apache_accesses', 'apache_processes', 'apache_volume' ]: }
   munin::plugin::deploy { 'apache_activity':
     source  => 'apache/munin/apache_activity',
-    seltype => 'munin_services_plugin_exec_t',
+    seltype => $seltype,
   }
 }
