523eb026b95c759da7c51ab2a05679297c19748d
mh <mh@immerda.ch>
2016-09-18 17:34:39 +0200

    fix logrotate on el7
    newer logroate versions want su if the log directory is writeable
    by others than the owner, which is the case for us, as the php
    processes must also write into these directories under their own
    name.
    If no su is set, logrotate will refuse to rotate. This change makes
    sure logrotate will rotate the apache logs, the php logs will be
    part of a second patch.

--- manifests/logrotate/centos.pp
+++ manifests/logrotate/centos.pp
@@ -1,10 +1,12 @@
-# add vhost folders to logrotation
+# remove vhost folders from logrotation
 class apache::logrotate::centos {
-    augeas{'logrotate_httpd':
-      changes => [ 'rm /files/etc/logrotate.d/httpd/rule/file',
-        'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
-        'set /files/etc/logrotate.d/httpd/rule/file[1] /var/log/httpd/*log' ],
-      onlyif  => 'get /files/etc/logrotate.d/httpd/rule/file[1] != "/var/log/httpd/*log"',
-      require => Package['apache'],
-    }
+  augeas{'logrotate_httpd':
+    incl    => '/etc/logrotate.d/httpd',
+    lens    => 'Logrotate.lns',
+    changes => [ 'rm /files/etc/logrotate.d/httpd/rule/file',
+      'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
+      'set /files/etc/logrotate.d/httpd/rule/file[1] /var/log/httpd/*log' ],
+    onlyif  => 'get /files/etc/logrotate.d/httpd/rule/file[1] != "/var/log/httpd/*log"',
+    require => Package['apache'],
+  }
 }
--- manifests/logrotate/centos/vhosts.pp
+++ manifests/logrotate/centos/vhosts.pp
@@ -1,11 +1,22 @@
-# add vhost folders to logrotation
+# add vhost folders to logrotation + su for newer logrotates
 class apache::logrotate::centos::vhosts inherits apache::logrotate::centos {
-    Augeas['logrotate_httpd']{
-      changes => [ 'rm /files/etc/logrotate.d/httpd/rule/file',
-        'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
-        'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
-        'set /files/etc/logrotate.d/httpd/rule/file[1] /var/log/httpd/*log',
-        'set /files/etc/logrotate.d/httpd/rule/file[2] /var/www/vhosts/*/logs/*log' ],
-      onlyif => 'get /files/etc/logrotate.d/httpd/rule/file[2] != "/var/www/vhosts/*/logs/*log"',
+  Augeas['logrotate_httpd']{
+    changes => [ 'rm /files/etc/logrotate.d/httpd/rule/file',
+      'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
+      'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
+      'set /files/etc/logrotate.d/httpd/rule/file[1] /var/log/httpd/*log',
+      'set /files/etc/logrotate.d/httpd/rule/file[2] /var/www/vhosts/*/logs/[aem]*log' ],
+    onlyif => 'get /files/etc/logrotate.d/httpd/rule/file[2] != "/var/www/vhosts/*/logs/[aem]*log"',
+  }
+  if versioncmp($::operatingsystemmajrelease,'6') > 0 {
+    augeas{'logrotate_httpd_su':
+      context => '/files/etc/logrotate.d/httpd/rule/',
+      incl    => '/etc/logrotate.d/httpd',
+      lens    => 'Logrotate.lns',
+      changes => [
+        'set su/owner root',
+        'set su/group root',
+      ]
     }
+  }
 }
