2b07ede89527e2d66366ba282b08337c7fcc8650
mh <mh@immerda.ch>
2020-09-20 11:19:12 +0200

    switch to one unified config

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -2,17 +2,17 @@
 class apache::munin(
   $pwd,
 ) {
-  if $::osfamily == 'Debian' {
+  if $facts['osfamily'] == 'Debian' {
     include perl::extensions::libwww
   }
 
-  if $::operatingsystem == 'CentOS' and versioncmp($::operatingsystemmajrelease,'6') > 0 {
+  if $facts['operatingsystem'] == 'CentOS' and versioncmp($facts['operatingsystemmajrelease'],'6') > 0 {
     $seltype = 'services_munin_plugin_exec_t'
   } else {
     $seltype = 'munin_services_plugin_exec_t'
   }
 
-  $config_str = "env.url   http://munin:${pwd}127.0.0.3:%d/server-status?auto\nenv.ports 80"
+  $config_str = "env.url   http://munin:${pwd}@127.0.0.3:%d/server-status?auto\nenv.ports 80"
   munin::plugin{
     [ 'apache_accesses', 'apache_processes', 'apache_volume' ]:
       config => $config_str;
