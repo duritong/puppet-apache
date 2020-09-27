b5d65c7586d4e0b92f348e2869ef5c203773aa58
mh <mh@immerda.ch>
2020-09-27 16:54:20 +0200

    drop support for modperl, typo3, silverstrip

--- manifests/include/silverstripe.pp
+++
@@ -1,6 +0,0 @@
-# deploy a default include for silverstripe
-class apache::include::silverstripe {
-  apache::config::include{'silverstripe.inc':
-    content => template('apache/include.d/silverstripe.inc.erb')
-  }
-}
