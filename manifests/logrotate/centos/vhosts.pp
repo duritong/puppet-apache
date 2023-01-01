7204bdee993a6971e8c2c5aee8ec5cc54c89ac79
mh <mh@immerda.ch>
2023-01-01 09:21:54 +0100

    be more specific about logs to rotate, so they don't overlap with others

--- manifests/logrotate/centos/vhosts.pp
+++ manifests/logrotate/centos/vhosts.pp
@@ -5,8 +5,8 @@ class apache::logrotate::centos::vhosts inherits apache::logrotate::centos {
       'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
       'ins file before /files/etc/logrotate.d/httpd/rule/*[1]',
       'set /files/etc/logrotate.d/httpd/rule/file[1] /var/log/httpd/*log',
-    'set /files/etc/logrotate.d/httpd/rule/file[2] /var/www/vhosts/*/logs/[aem]*log'],
-    onlyif => 'get /files/etc/logrotate.d/httpd/rule/file[2] != "/var/www/vhosts/*/logs/[aem]*log"',
+    'set /files/etc/logrotate.d/httpd/rule/file[2] /var/www/vhosts/*/logs/{access,error,mod_security}*log'],
+    onlyif => 'get /files/etc/logrotate.d/httpd/rule/file[2] != "/var/www/vhosts/*/logs/{access,error,mod_security}*log"',
   }
   if versioncmp($facts['os']['release']['major'],'6') > 0 {
     augeas { 'logrotate_httpd_su':
