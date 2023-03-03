8fd2952fb2f6bd48699fea008047014df6319436
mh <mh@immerda.ch>
2023-03-03 22:55:00 +0100

    pull in all the deps if a user is added

--- manifests/module/authz_pam/allow_user.pp
+++ manifests/module/authz_pam/allow_user.pp
@@ -3,6 +3,7 @@ define apache::module::authz_pam::allow_user (
   String[1] $user = $title,
   String[1] $pam_service = 'httpd',
 ) {
+  include apache::module::authz_pam
   concat::fragment { "${pam_service}_pam_allow_${name}":
     target  => "/etc/httpd/conf.d/pam-${pam_service}-allow-users",
     content => "${user}\n",
