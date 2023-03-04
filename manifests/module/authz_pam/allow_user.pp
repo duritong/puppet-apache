6d77fbc4e24e319ba2d664f47a5e5dd097aa8857
mh <mh@immerda.ch>
2023-03-04 21:36:37 +0100

    adjust also here the path

--- manifests/module/authz_pam/allow_user.pp
+++ manifests/module/authz_pam/allow_user.pp
@@ -5,7 +5,7 @@ define apache::module::authz_pam::allow_user (
 ) {
   include apache::module::authz_pam
   concat::fragment { "${pam_service}_pam_allow_${name}":
-    target  => "/etc/httpd/conf.d/pam-${pam_service}-allow-users",
+    target  => "/etc/httpd/conf.modules.d/pam-${pam_service}-allow-users",
     content => "${user}\n",
     order   => '10';
   }
